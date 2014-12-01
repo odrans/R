e1a6fc34397af01a7504fcaeb600cad37020d26e
odrans <odran.sourdeval@gmail.com>
2014-12-01 15:50:59 +0100

    modified:   RLIMsat/R/plot.projmap.R

--- RLIMsat/R/plot.projmap.R
+++ RLIMsat/R/plot.projmap.R
@@ -52,6 +52,14 @@ map.plot <- function(Lat,Lon,Prod,proj="eck4",sampling="cubic",interp=TRUE,overw
 
     sdf.proj[[name.prod]] <- postprocess(sdf.proj[[name.prod]])
     sdf.proj[[name.prod]] <- replace(sdf.proj[[name.prod]],is.nan(sdf.proj[[name.prod]]),NA)
+
+    sdf.proj[[name.prod]] <- replace(sdf.proj[[name.prod]],is.nan(sdf.proj[[name.prod]]),NA)
+
+    options <- list(...)
+    idx.zlim <- which(names(options)=="zlim"); zlim <- options[[idx.zlim]]
+
+    sdf.proj[[name.prod]] <- replace(sdf.proj[[name.prod]],sdf.proj[[name.prod]]<zlim[1],zlim[1])
+    sdf.proj[[name.prod]] <- replace(sdf.proj[[name.prod]],sdf.proj[[name.prod]]>zlim[2],zlim[2])
     
     image.plot(sdf.proj,nlevel=ncol,col=color,...)
     mapCountryData(map.proj,numCats=1,addLegend=FALSE,colourPalette = c("grey88","grey88"),add=T,borderCol = "black",mapTitle="")
