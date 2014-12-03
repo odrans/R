f1dff036b1cf4981e00340a7a11b37943f15195e
odrans <odran.sourdeval@gmail.com>
2014-12-03 22:25:35 +0100

    modified:   RLIMsat/R/plot.projmap.R

--- RLIMsat/R/plot.projmap.R
+++ RLIMsat/R/plot.projmap.R
@@ -45,7 +45,8 @@ map.plot <- function(Lat,Lon,Prod,proj="eck4",sampling="cubic",interp=TRUE,overw
     }
 
     if(overwrite) sdf.latlon <- create.sdf.fast(Lat,Lon,Prod,box.size,name.prod)
-
+    str(sdf.latlon)
+    stop()
     sdf.proj <- project.sdf(sdf.latlon,proj,sampling,dir.GTiff,interp,overwrite,name.prod)
     map.proj <- project.map(proj)
 
